2b4fc3fd391dee0c246185fb563bb04f7f9def4e
Shandy Brown <github@ezide.com>
2017-02-23 08:14:41 -0800

    basic working status.  dimensions are wrong

---
+++ downtown/process.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import re
+from lxml import etree
+from itertools import product
+
+from sheet_parser import load_csv, parse
+
+def export_png(svg, png):
+    cmd_fmt = 'inkscape --export-png=%s --export-width=400 %s'
+    cmd = cmd_fmt % (png, svg)
+    print cmd
+    os.system(cmd)
+
+class DOM(object):
+    def __init__(self, svg_file):
+        fp = file(svg_file)
+        c = fp.read()
+        fp.close()
+        self.dom = etree.fromstring(c)
+        self.titles = [x for x in self.dom.getiterator()
+                       if x.tag == '{http://www.w3.org/2000/svg}title']
+        self.title_to_element = {
+            t.text: t.getparent()
+            for t in self.titles
+        }
+
+    def cut_element(self, title):
+        e = self.title_to_element[title]
+        e.getparent().remove(e)
+
+    def add_rectangle(self, rect):
+        trect = self.title_to_element['template_rect']
+        group = trect.getparent()
+        group.append(rect)
+
+    def replace_text(self, title, newtext, max_chars=None):
+        flowroot = self.title_to_element[title]
+        flowpara = [x for x in flowroot.iterchildren() if 'flowPara' in x.tag][0]
+        flowroot.remove(flowpara)
+        for i, line in enumerate(newtext.split('\n')):
+            paraclone = etree.fromstring(etree.tostring(flowpara))
+            paraclone.text = line
+            flowroot.append(paraclone)
+        num_lines = i
+
+        if max_chars and len(newtext) > (max_chars - num_lines*20):
+            flowroot.attrib['style'] = re.sub(
+                'font-size:\d+px;', 'font-size:8px;', flowroot.attrib['style']
+            )
+
+    def write_file(self, svg_filename):
+        print svg_filename
+        fp = file(svg_filename, 'w')
+        fp.write(etree.tostring(self.dom))
+        fp.close()
+
+def defang(s):
+    return s.replace('&','_').replace('<','_').replace('>','_')
+
+
+def make_rectangle(name, i, dimensions, lastx, lasty):
+    px_to_cm = 35.433
+    if lastx > 600:
+        y = 200 * (i%5)
+        x = 0
+    else:
+        y = lasty
+        x = lastx + 50
+    width = dimensions[0] * px_to_cm
+    height = dimensions[1] * px_to_cm
+    rstr = '''
+    <g>
+    <rect style="fill:#ff0000;fill-opacity:0.5;stroke:none;"
+       width="%s"
+       height="%s"
+       x="%s"
+       y="%s"
+       />
+    <text
+       style="font-size:12px;font-family:sans-serif;fill:#000000;stroke:none;"
+       x="%s"
+       y="%s"
+       ><tspan>%s</tspan></text>
+    </g>
+    ''' % (width, height, x, y,
+           x, y+13, defang(name[:10]))
+    print rstr
+    endx = x + width
+    endy = y
+    return etree.fromstring(rstr), endx, endy
+
+
+def make_map():
+    csv_reader = load_csv()
+    rectdata = parse(csv_reader)
+
+    export_png('base_map.svg', '/tmp/new_map/back.png')
+
+    dom = DOM('base_map.svg')
+
+    lastx, lasty = 0, 0
+
+    for i, (name, dimensions) in enumerate(rectdata):
+
+        print '\nWorking on %s %s' % ( i , name )
+        print '\n'
+
+        rect, lastx, lasty = make_rectangle(name, i, dimensions, lastx, lasty)
+
+        dom.add_rectangle(rect)
+
+    # Create the svg file and export a PNG
+    svg_filename = '/tmp/new_map/new_map.svg'
+    png_filename = '/tmp/new_map/new_map.png'
+
+    dom.write_file(svg_filename)
+
+    export_png(svg_filename, png_filename)
+
+
+if __name__ == '__main__':
+    if not os.path.exists('/tmp/new_map'):
+        os.makedirs('/tmp/new_map')
+    make_map()
--- downtown/sheet_parser.py
+++ downtown/sheet_parser.py
@@ -3,15 +3,55 @@
 import csv
 import requests
 
+
+# Shandy's Test URL
 URL = 'https://docs.google.com/spreadsheets/d/1MhY-qCFE4gyxfwpFjnA0bM6uoi8ZFI6AKhJjBXT8F9E/pub?gid=789892116&single=true&output=csv'
 
-csv_content = requests.get(URL).content
+# Live URL
+URL = 'https://docs.google.com/spreadsheets/d/1KuIeWepieysZf-f4C0FkB3oa1V9jGKe6DilThTNYdEE/pub?gid=0&single=true&output=csv'
 
-lines = csv_content.split('\r\n')
+def load_csv():
+    csv_content = requests.get(URL).content
 
-reader = csv.DictReader(lines)
+    lines = csv_content.split('\r\n')
 
-for item in reader.next():
-    print item
+    # ignore empty lines
+    lines = [x for x in lines
+             if x.strip(',')]
+
+    reader = csv.DictReader(lines)
+
+    return reader
+
+def get_dims(cell_value):
+    dims = cell_value.strip()
+    if dims:
+        try:
+            dims = [float(measure) for measure in dims.split('x')]
+            return dims
+        except ValueError:
+            print 'Had trouble with %s (%s)' % (m_dims)
+
+def parse(reader):
+    result = []
+    for item in reader:
+        m_dims = get_dims(item['Dimensions (metric)'])
+        if not m_dims:
+            i_dims = get_dims(item['Dimensions (imperial)'])
+            if i_dims:
+                m_dims = [measure * 0.305 for measure in i_dims]
+
+        if not m_dims:
+            print 'Had trouble with: ', item['Use']
+        else:
+            result.append([item['Use'], m_dims])
+    return result
 
 
+def main():
+    csv_reader = load_csv()
+    rectangles = parse(csv_reader)
+    print rectangles
+
+if __name__ == '__main__':
+    main()
